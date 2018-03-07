007af71b92cc4182fb5c9d69221ac092e3872dca
rc <rfabian@bizbox.ph>
2018-03-07 17:06:14 +0800

    has user locator

--- App.js
+++ App.js
@@ -8,18 +8,63 @@ import MapView from 'react-native-maps';
 
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      region: {
+        latitude: 14.5547,
+        longitude: 121.0244,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      },
+      marker:{
+        latitude: 14.5547,
+        longitude: 121.0244,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }
+    }
+  }
+
+  componentWillMount(){
+    navigator.geolocation.getCurrentPosition((position)=> {
+      this.setState({
+        region:{
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        },
+        marker: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      })
+    },
+    (error) => console.log(error,'eeerrrrroooooorr'),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+  }
+
+
   render() {
     return (
       <ThemeProvider>
         <View style = {styles.container}>
             <MapView style = {styles.map} 
-              region={{
+              region={this.state.region}
+            >
+            <MapView.Marker 
+                coordinate={{
                 latitude: 14.5547,
                 longitude: 121.0244,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1
-              }}
-            />
+                }}
+                title='sample marker'
+              />
+               <MapView.Marker 
+                coordinate={this.state.marker}
+                title='youre here'
+              />
+            </MapView>
           <Button primary text="Primary" />
         </View>
       </ThemeProvider>
