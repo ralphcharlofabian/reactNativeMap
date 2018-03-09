24acc54ec7eea196f3046d307db9b8fcbf8dce54
rc <rfabian@bizbox.ph>
2018-03-09 17:01:32 +0800

    new aproach to todolist

--- module/chatMessages/conversation.js
+++ module/chatMessages/conversation.js
@@ -1,31 +1,72 @@
 import React from 'react';
 
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, ListView, TouchableHighlight } from 'react-native';
 import { StackNavigation } from 'react-navigation';
 import { ThemeProvider, Button } from 'react-native-material-ui';
 
 
 export default class Conversation extends React.Component { 
+
+    constructor(props){
+        super(props);
+        let ds = new ListView.DataSource({
+            rowHasChanged:(r1,r2) => r1 !== r2
+        });
+        this.state ={
+            itemDataSource: ds
+        }
+
+        this.renderRow = this.renderRow.bind(this);
+        this.pressRow = this.pressRow.bind(this);
+    }
+
+    componentWillMount(){
+        this.getItems();
+    }
+
+    componentDidMount(){
+        this.getItems();
+    }
+
+    getItems(){
+        let items =[{title:'itemOne'},{title:'item two'}];
+
+        this.setState({
+            itemDataSource: this.state.itemDataSource.cloneWithRows(items)
+        });
+    }
+
+    pressRow(item){
+        
+    }
+
+    renderRow(item){
+        return(
+            <TouchableHighlight onPress={() => {
+                this.pressRow(item);
+            }}>
+                <View>
+                    <Text>
+                        {item.title}
+                    </Text>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+
     render () {
 
         const { params } = this.props.navigation.state;
         var name = params.userName; 
         return(
-            <View style={{ flex: 1, alignItems: 'left', justifyContent: 'left' }}>
+            <View style={{ flex: 1, alignItems: 'top', justifyContent: 'top' }}>
                  <Text>{JSON.stringify(name) } test</Text>
                 
+                <ListView 
+                    dataSource={this.state.itemDataSource}
+                    renderRow = {this.renderRow} 
+                />
             </View>
         );
     }
 }
-{/* <GiftedChat
-                    messages={this.state.messages}
-                    onSend={(message)=>{
-                        //for backend https://www.youtube.com/watch?v=7Ot5LSGHLyY reference https://github.com/FaridSafi/react-native-gifted-chat
-                        // https://www.youtube.com/watch?v=915vvTQV_tI reference https://school.shoutem.com/lectures/chat-app-firebase-redux-react-native/
-                        //
-                    }}
-                    user={{
-                      _id: 1,
-                    }}
-      /> */}
